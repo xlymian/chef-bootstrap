b5d9529d44f8d7a32d49dc4760d6f4f2ee8f6c8a
Paul Mylchreest <paul.mylchreest@mac.com>
2009-10-01 17:45:54 -0400

    install aws-s3 gem

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,8 +1,5 @@
 #!/usr/bin/env ruby
 
-# Run this before running the bootstrap: 
-#   aptitude -y update && apt-get -y install git-core
-
 CHEF_550 = true # Until CHEF-550 is merged into master
 
 def cmd(cmd)
@@ -13,12 +10,13 @@ puts '-' * 80
 puts "Chef bootstrap"
 puts
 
+cmd "aptitude -y update"
+cmd "apt-get -y install git-core"
 cmd "aptitude -y install irb ri rdoc libshadow-ruby1.8 ruby1.8-dev gcc g++ curl"
 cmd "curl -L 'http://rubyforge.org/frs/download.php/45905/rubygems-1.3.1.tgz' | tar xvzf -"
 cmd "cd rubygems* && ruby setup.rb --no-ri --no-rdoc"
 cmd "ln -sfv /usr/bin/gem1.8 /usr/bin/gem"
-
-cmd "gem install rdoc chef ohai --no-ri --no-rdoc --source http://gems.opscode.com --source http://gems.rubyforge.org"
+cmd "gem install rdoc chef ohai aws-s3 --no-ri --no-rdoc --source http://gems.opscode.com --source http://gems.rubyforge.org"
 
 if CHEF_550
   cmd %q{sed -i 's/Chef::Config\[:file_cache_path\]/::File.expand_path(Chef::Config\[:file_cache_path\])/' /usr/lib/ruby/gems/1.8/gems/chef-0.7.10/lib/chef/provider/template.rb}
