7d45661200bf117036a09c64b247d54412db84a8
Paul Mylchreest <paul.mylchreest@mac.com>
2009-10-01 17:55:51 -0400

    test

--- bootstrap.rb
+++ bootstrap.rb
@@ -10,8 +10,8 @@ puts '-' * 80
 puts "Chef bootstrap"
 puts
 
-cmd "aptitude -y update"
-cmd "apt-get -y install git-core"
+# cmd "aptitude -y update && apt-get -y install git-core"
+
 cmd "aptitude -y install irb ri rdoc libshadow-ruby1.8 ruby1.8-dev gcc g++ curl"
 cmd "curl -L 'http://rubyforge.org/frs/download.php/45905/rubygems-1.3.1.tgz' | tar xvzf -"
 cmd "cd rubygems* && ruby setup.rb --no-ri --no-rdoc"
